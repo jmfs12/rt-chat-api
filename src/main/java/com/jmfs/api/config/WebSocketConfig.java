4052ae35d872ddf7d5c38489b1d4b8a427f5a328
João Miguel <joanomiguel@gmail.com>
2025-08-08 22:14:19 -0300

    fix: @EnableWebSocketMessageBroker annotation added

--- src/main/java/com/jmfs/api/config/WebSocketConfig.java
+++ src/main/java/com/jmfs/api/config/WebSocketConfig.java
@@ -2,12 +2,12 @@ package com.jmfs.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
